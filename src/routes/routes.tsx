cf747fa35484c2ca29ce4c5c9dd8104be0959972
Егор Кириенко <egor.keen@mail.ru>
2024-11-24 10:01:42 +0300

    feature: добавил главную страницу

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "../app";
 import { routerPath } from "utils/types";
+import { Main } from "pages";
 
 export const router = createBrowserRouter([
   {
     path: routerPath.MAIN,
     element: <App />,
     children: [
+      {
+        path: routerPath.MAIN,
+        element: <Main />,
+      },
       {
         path: routerPath.TITLE,
         element: null,
