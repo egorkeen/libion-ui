cef3c7d373f64658ef1a64083993547d697f8ea4
Егор Кириенко <egor.keen@mail.ru>
2024-11-24 09:23:28 +0300

    chore: добавил alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      routes: path.resolve(__dirname, "src/routes"),
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      utils: path.resolve(__dirname, "src/utils"),
+      assets: path.resolve(__dirname, "src/assets"),
+      store: path.resolve(__dirname, "src/store"),
+    },
+  },
+});
